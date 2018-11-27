a684235d48ed99d3c5f708ae96a15398eaf5a044
ryu022304 <ryu022304@gmail.com>
2018-11-27 22:40:26 +0900

    [update]manage.pyでstartapp実行まで

--- NLP_100knocks_69/settings.py
+++ NLP_100knocks_69/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nlp_100knocks_69_webapp'
 ]
 
 MIDDLEWARE = [
---
+++ nlp_100knocks_69_webapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Nlp100Knocks69WebappConfig(AppConfig):
+    name = 'nlp_100knocks_69_webapp'
